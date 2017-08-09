e8a8835548f1338b8d7b339db01b9bc6ef76e060
mariya-kos <mariyakosacheva@gmail.com>
2017-08-10 02:46:54 +0300

    add form handler

--- form_handler/views.py
+++ form_handler/views.py
@@ -2,29 +2,33 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from form_handler.forms import ContactForm
+from django.core.mail import EmailMultiAlternatives
 
 def email(request):
-
-    print(request.POST)
 
     if request.method == 'GET':
         form = ContactForm()
     else:
         form = ContactForm(request.POST)
-        print(form)
         if form.is_valid():
             theme = form.cleaned_data['theme']
             email = form.cleaned_data['email']
             text = form.cleaned_data['name'] + '\n'
             text += form.cleaned_data['text']
             try:
-                send_mail(theme, text, email, ['mariyakosacheva@gmail.com'])
+
+
+                msg = EmailMultiAlternatives(theme, text, email, ['mariyakosacheva@gmail.com'])
+                email_content = '...'
+                msg.attach_alternative(email_content, "text/html")
+                msg.send()
+                # send_mail(theme, text, email, ['mariyakosacheva@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('thanks')
         else:
-            print("form is not valid")
-    return render(request, "ru/ji", {'form': form})
+            print("Form is not valid")
+    return render(request, "form_handler/email.html", {'form': form})
 
 def thanks(request):
     return HttpResponse('Thank you for your message.')
