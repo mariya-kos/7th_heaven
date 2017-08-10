8045e118749d590411a4bf32cd4e10ebf0add93c
mariya-kos <mariyakosacheva@gmail.com>
2017-08-10 13:41:20 +0300

    rooms list script testing

--- mysite/static/js/rooms_list.js
+++ mysite/static/js/rooms_list.js
@@ -3,9 +3,8 @@ $(".rooms dt").on("click", function () {
     $(".selected_room_desc").removeClass("selected_room_desc");
     var selected_room = $(".selected_room");
     if (selected_room && !$(this).hasClass("selected_room")) {
-        console.log("this is not selected yet");
         selected_room.removeClass("selected_room");
     }
     $(this).toggleClass("selected_room");
-    $(this).next("dd").addClass("selected_room_desc");
+    $("selected_room").next("dd").addClass("selected_room_desc");
 });
