c236e1ae9a603ee19253128d1bcb969b9bbc228a
mariya-kos <mariyakosacheva@gmail.com>
2017-08-10 20:00:29 +0300

    add testing js

--- mysite/static/js/rooms_list.js
+++ mysite/static/js/rooms_list.js
@@ -12,29 +12,28 @@ $(window).on("load", function () {
         $(this).append('<div class="cmsplugin_filer_folder_list room_imgs">' + html + '</div>');
         $(".room_imgs img").each(function () {
             $(this).on("click", function () {
-                console.log('clicked')
                  $(this).parent("li").addClass("selected_img");
                  var src = $(this).attr("src");
                  var elem = "<div id='img_view'><div class='container'><img src='" + src + "'/></div></div>";
 
                 $("body").append(elem);
                 $("#img_view").on("click", function (e) {
-                    var offset = $(this).offset();
-                    var x = e.pageX - offset.left;
-                    var y = e.pageY - offset.top;
-                    var img = $(this).children("div").children("img");
-                    if (img.offset().left < x && (img.offset().left + img.width()) > x) {
-                        var next_selected_elem = $(".selected_img").next();
-                        if (!next_selected_elem[0]) {
-                            next_selected_elem = $(".cmsplugin_filer_folder_list li");
-                        }
-                        $(".selected_img").removeClass("selected_img");
-                        next_selected_elem.addClass("selected_img");
-                        img.attr("src", $(".selected_img").children().attr("src"));
-                    }
-                    else {
+                    // var offset = $(this).offset();
+                    // var x = e.pageX - offset.left;
+                    // var y = e.pageY - offset.top;
+                    // var img = $(this).children("div").children("img");
+                    // if (img.offset().left < x && (img.offset().left + img.width()) > x) {
+                    //     var next_selected_elem = $(".selected_img").next();
+                    //     if (!next_selected_elem[0]) {
+                    //         next_selected_elem = $(".cmsplugin_filer_folder_list li");
+                    //     }
+                    //     $(".selected_img").removeClass("selected_img");
+                    //     next_selected_elem.addClass("selected_img");
+                    //     img.attr("src", $(".selected_img").children().attr("src"));
+                    // }
+                    // else {
                         $(this).remove();
-                    }
+                    // }
                 });
             });
         });
