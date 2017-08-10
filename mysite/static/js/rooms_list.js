44b48a58d4f92fc4895aa27da1210f8bce6322b3
mariya-kos <mariyakosacheva@gmail.com>
2017-08-10 13:37:07 +0300

    change rooms list style

--- mysite/static/js/rooms_list.js
+++ mysite/static/js/rooms_list.js
@@ -3,6 +3,7 @@ $(".rooms dt").on("click", function () {
     $(".selected_room_desc").removeClass("selected_room_desc");
     var selected_room = $(".selected_room");
     if ($(this) !== selected_room) {
+        console.log("this is not selected yet");
         selected_room.removeClass("selected_room");
     }
     $(this).toggleClass("selected_room");
